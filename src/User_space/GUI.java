e03b18f8ba7fd26abaa07faf91a384e63aac9c21
ibrahimramadan <ramadanig@mymail.vcu.edu>
2017-12-01 02:49:45 -0500

    Added VM class and put so when you are on the GUI you can get previously submitted commands with the up keyboard button and added user guide.

--- src/User_space/GUI.java
+++ src/User_space/GUI.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
-
+import java.util.Stack;
 
 /**
  * @author Will Russell on 11/8/17
@@ -110,12 +110,14 @@ public class GUI extends Application {
         newProcessesTextArea.setPrefColumnCount(45);
         newProcessesTextArea.autosize();
 
-
+        Stack prev = new Stack();
+        String prevCmd;
         button = new Button();
         button.setText("Submit");
         button.setOnAction(event -> {
             boolean valid = false;
             String inputText = textInput.getText();
+            prev.push(inputText);
             if (!inputText.trim().equals(""))
                 valid = CLI.isValidCommand(textInput.getText());
             textInput.clear();
@@ -130,7 +132,18 @@ public class GUI extends Application {
         controls = new HBox();
         controls.setSpacing(10);
         controls.getChildren().addAll(textInput, button);
+        controls.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+            if (event.getCode().equals(KeyCode.UP)) {
+                if(!prev.empty()) {
+                    textInput.setText(prev.pop().toString());
+                }
+                else{
+                    //do Nothing
+                }
+            }
 
+
+        });
 
         root = new BorderPane();
 
